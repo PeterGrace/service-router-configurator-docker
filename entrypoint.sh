97f087f25d71adda1b34249d0c4240f4ff646175
Peter Grace <pete.grace@gmail.com>
2017-12-12 14:31:38 +0000

    etcdctl no longer exits on watch, so, lets watch a fifo instead; change path to service-router-configurator

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
+FIFO=${SVC_FIFO:-svc_fifo}
+
 kubectl config set-credentials $K8S_CLUSTER_USER --token=$K8S_CLUSTER_TOKEN
 kubectl config set-cluster $K8S_CLUSTER_NAME --server=$K8S_CLUSTER_URL --certificate-authority=/ca.crt
 kubectl config set-context default --cluster=$K8S_CLUSTER_NAME --user=$K8S_CLUSTER_USER
 kubectl config use-context default
 
-while true
+
+mkfifo $FIFO
+ETCDCTL_API=3 etcdctl --endpoints "$ETCDSERVERS" watch "/registry/services/specs" --prefix=true  --interactive=false >> $FIFO 2>&1 &
+while read line
 do
-    ETCDCTL_API=3 etcdctl --endpoints "$ETCDSERVERS" watch "/registry/services/specs" --prefix=true >/dev/null \
-        && echo "$(date): Kubernetes service updated" \
-        && /usr/local/bin/service-router-configurator --etcd-host "$ETCDSERVERS" --etcd-path /service-router/haproxy-config apply
-done
+        echo "$(date): Kubernetes service updated" \
+        && /usr/bin/service-router-configurator --etcd-host "$ETCDSERVERS" --etcd-path /service-router/haproxy-config apply
+done < $FIFO
